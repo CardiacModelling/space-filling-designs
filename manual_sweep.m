439caf49b7dd8c8237b455f43ea4c8fb72c5ebd5
Gary Mirams <gary.mirams@gmail.com>
2023-11-10 12:00:41 +0000

    Update manual sweep to match latest options

--- manual_sweep.m
+++ manual_sweep.m
@@ -1,7 +1,7 @@
 function best_params = manual_sweep(ICs, box_hits, Model_Params)
 
 durations = [25 50 100 200 500 1000]';
-voltages = linspace(-120,60,6)';
+voltages = linspace(-115,55,6)';
 
 best_score = 10000;
 
@@ -13,7 +13,7 @@ for i = 1:length(durations)
                     for n = 1:length(voltages)
                         fprintf('%i, %i, %i, %i, %i, %i\n',i,j,k,l,m,n)
                         step_params = [durations(i) voltages(j) durations(k) voltages(l) durations(m) voltages(n)];
-                        score = step_param_objective(step_params,ICs,box_hits,Model_Params);
+                        score = step_param_objective(step_params,ICs,box_hits,Model_Params,true);
 
                         if score < best_score
                             best_score = score;
